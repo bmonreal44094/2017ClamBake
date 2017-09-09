2a35f5c8cf0d7aa895ef42e92d2937f84aefd989
MCC\bmonreal <bmonreal@gremlinA>
2017-09-09 18:10:15 -0400

    need to add email response with reservation

---
+++ assets/javascript/party.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBPXIhY9DdtCYf-y4q3GYLz_giViRoRAxc",
+    authDomain: "clambake-88288.firebaseapp.com",
+    databaseURL: "https://clambake-88288.firebaseio.com",
+    projectId: "clambake-88288",
+    storageBucket: "",
+    messagingSenderId: "1041369538927"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+  
