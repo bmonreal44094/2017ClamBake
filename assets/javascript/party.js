882b295d0f7c9a51f6b96efef6f0755411e3e8d8
Bill Monreal <BMonreal@mcc.local>
2017-09-18 23:34:25 -0400

    current working copy

--- assets/javascript/party.js
+++ assets/javascript/party.js
@@ -1,4 +1,18 @@
 // Initialize Firebase
+var bake = {
+  steakBakePrice : 44,
+  chickenBakePrice : 38,
+  extraClamsPrice : 10,
+  steakBake:0,
+  chickenBake:0,
+  extraClams:0,
+  total:0,
+  TotalSum :function(){
+    bake.total = bake.steakBake * bake.steakBakePrice
+    + bake.chickenBake* bake.chickenBakePrice     + bake.extraClams * bake.extraClamsPrice;
+  },
+
+};
 var config = {
   apiKey: "AIzaSyBPXIhY9DdtCYf-y4q3GYLz_giViRoRAxc",
   authDomain: "clambake-88288.firebaseapp.com",
@@ -10,30 +24,80 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-$(document).ready(function() {
+$("#bake-order-btn").click(function(event){
+    event.preventDefault();
+//     console.log(1);
+// });
 
-  $("#sign-up-form").on('submit', function(e) {
-    e.preventDefault();
+// $("#sign-up-form").on('submit', function(event) {
+//   event.preventDefault();
 
-    form = $(this);
-    form.parsley().validate();
+  // form = $("#sign-up-form");
+  // console.log(form);
+  // form.parsley().validate();
 
-    if(form.parsley().isValid()) {
+  // if(form.parsley().isValid()) {
 
-      //Whatever happens when the form is valid
+    //Whatever happens when the form is valid
+    var firstName = $('#first-name-input').val();
+    var lastName = $('#last-name-input').val();
+    var email = $('#email-input').val();
+    var phone = $('#phone-input').val();
+    // var steakBakes = parseInt($("#steak-bakes-input1").val());
+    // var chickenBakes = parseInt($("#chicken-bakes-input1").val());
+    // var extraClams = parseInt($("#extra-clams-input1").val());
+    // // var steakBakePrice = 44;
+    // var chickenBakePrice = 38;
+    // var extraClamsPrice = 10;
+    // var steakBakeTTL = steakBakes * steakBakePrice;
+    // var chickenBakeTTL = chickenBakes * chickenBakePrice;
+    // var extraClamsTTL = extraClams * extraClamsPrice;
+    // var totalTTL = steakBakeTTL + chickenBakeTTL + extraClamsTTL
 
-      var firstName = $('#first-name-input'),
-          lastName = $('#last-name-input'),
-          email = $('#email-input'),
-          phone = $('#phone-input'); 
-          //etc.
 
-     
+//ONCE THE REST IS WORKING UNCOMMNET THIS SECTION
+    // emailjs.init("user_3nudfluVS4TNdqBpf3Gf3");
+    //   emailjs.send("default_service","2017_clambake",{
+    //     name:firstName + " " + lastName,
+    //     email:email,
+    //     steak_bake:steakBakes,
+    //     steak_bake_price:steakBakePrice,
+    //     steak_bake_total:steakBakeTTL,
+    //     chicken_bakes:chickenBakes,
+    //     chicken_bakes_price:chickenBakePrice,
+    //     chicken_bake_total:chickenBakeTTL,
+    //     extra_clams:extraClams,
+    //     extra_clams_price:extraClamsPrice,
+    //     extra_clams_total:extraClamsTTL,
+    //     total_price:totalTTL,
+    //});
 
-    } else {
-      alert("Invalid Form");//This can't really happen, but I put it in there anyway
-    }
-  });
-  
-});
-  
+     // window.location.reload(false); 
+//   } else {
+//      alert("Invalid Form");//This can't really happen, but I put it in there anyway
+//    }
+ });
+
+$("#steak-bakes-input1").on("change keyup paste", function(){
+    bake.steakBake = parseInt($("#steak-bakes-input1").val());
+    bake.TotalSum();
+    $("#steak-bakes-qty").text("Bakes: " + bake.steakBake);
+    $("#steak-bakes-ttl").text("Steak Total: $" + bake.steakBake*bake.steakBakePrice);
+    $("#total-ttl").text("$" + bake.total);
+    });
+
+$("#chicken-bakes-input1").on("change keyup paste", function(){
+    bake.chickenBake = parseInt($("#chicken-bakes-input1").val());
+    bake.TotalSum();
+    $("#chicken-bakes-qty").text("Bakes: " + bake.chickenBake);
+    $("#chicken-bakes-ttl").text("Chicken Total: $" + bake.chickenBake*bake.chickenBakePrice);
+    $("#total-ttl").text("$" + bake.total);
+    });
+
+$("#extra-clams-input1").on("change keyup paste", function(){
+    bake.extraClams = parseInt($("#extra-clams-input1").val());
+    bake.TotalSum();
+    $("#extra-clams-qty").text("Extra Dozen Clams: " + bake.extraClams);
+    $("#extra-clams-ttl").text("Extra Clam Total: $" + bake.extraClams*bake.extraClamsPrice);
+    $("#total-ttl").text("$" + bake.total);
+    });
